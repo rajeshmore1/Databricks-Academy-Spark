13b492b25e037d94175dd250b8f8f5a7d303f6ef
SireInsectus <SireInsectus>
2022-01-12 20:22:49 +0000

    Removed v1.6.2

--- Spark-Programming-1.6.2-IL/Version-1.6.2-IL.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-// MAGIC 
-// MAGIC %md # Course: Spark-Programming
-// MAGIC * Version 1.6.2-IL
-// MAGIC * Built 2021-10-26 02:59:26 UTC
-// MAGIC * Git revision: d77c12ce8d73db84e0be06833e3f4f0131b986f0
-// MAGIC 
-// MAGIC Copyright © 2021 Databricks, Inc.
